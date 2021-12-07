698f39dc99a7a8653894cf1d8b1bfa985f6bdc56
Caspar Schwarz-Schilling <31305984+casparschwa@users.noreply.github.com>
2021-12-07 10:16:34 +0100

    WIP sandwich test

--- tests/core/pyspec/eth2spec/test/phase0/fork_choice/test_ex_ante.py
+++ tests/core/pyspec/eth2spec/test/phase0/fork_choice/test_ex_ante.py
@@ -49,22 +49,30 @@ def test_ex_ante_scenario_1_with_boost(spec, state):
     yield 'anchor_state', state
     yield 'anchor_block', anchor_block
     current_time = state.slot * spec.config.SECONDS_PER_SLOT + store.genesis_time
+    print("HWW test || initialization time: ", current_time)
     on_tick_and_append_step(spec, store, current_time, test_steps)
     assert store.time == current_time
 
     # On receiving block A at slot `N`
     yield from _apply_base_block_a(spec, state, store, test_steps)
+    print("received A time: ", current_time)
     state_a = state.copy()
 
     # Block B at slot `N + 1`, parent is A
     state_b = state_a.copy()
     block = build_empty_block(spec, state_a, slot=state_a.slot + 1)
     signed_block_b = state_transition_and_sign_block(spec, state_b, block)
+    print("B's slot is: ", signed_block_b.message.slot)
+    print("parent root: ", signed_block_b.message.parent_root)
+    print("root (block B): ", signed_block_b.message.hash_tree_root().hex())
 
     # Block C at slot `N + 2`, parent is A
     state_c = state_a.copy()
     block = build_empty_block(spec, state_c, slot=state_a.slot + 2)
     signed_block_c = state_transition_and_sign_block(spec, state_c, block)
+    print("C's slot is: ", signed_block_c.message.slot)
+    print("parent root: ", signed_block_c.message.parent_root)
+    print("root (block C): ", signed_block_c.message.hash_tree_root())
 
     # Attestation_1 received at N+2 — B is head due to boost proposer
     def _filter_participant_set(participants):
@@ -79,17 +87,29 @@ def test_ex_ante_scenario_1_with_boost(spec, state):
 
     # Block C received at N+2 — C is head
     time = state_c.slot * spec.config.SECONDS_PER_SLOT + store.genesis_time
+    print("C received: ", time)
     on_tick_and_append_step(spec, store, time, test_steps)
+    print("slot (received C): ", spec.get_current_slot(store))
+    print("head view: ", spec.get_head(store))
+    print("before block C: get_head(store)", spec.get_head(store))
+    print("before block C: signed_block_c.message.hash_tree_root()", signed_block_c.message.hash_tree_root())
     yield from add_block(spec, store, signed_block_c, test_steps)
+    print("after block C: get_head(store)", spec.get_head(store))
+    print("after block C: signed_block_c.message.hash_tree_root()", signed_block_c.message.hash_tree_root())
     assert spec.get_head(store) == signed_block_c.message.hash_tree_root()
 
     # Block B received at N+2 — C is head that has higher proposer score boost
     yield from add_block(spec, store, signed_block_b, test_steps)
+    print("B received: ", time)
+    print("slot (received B): ", spec.get_current_slot(store))
+    print("head view: ", spec.get_head(store))
     assert spec.get_head(store) == signed_block_c.message.hash_tree_root()
 
     # Attestation_1 received at N+2 — C is head
     yield from add_attestation(spec, store, attestation, test_steps)
     assert spec.get_head(store) == signed_block_c.message.hash_tree_root()
+
+    print(store.abc)
 
     yield 'steps', test_steps
 
@@ -325,5 +345,93 @@ def test_ex_ante_attestations_is_greater_than_proposer_boost_without_boost(spec,
     # Attestation_1 received at N+2 — B is head because B's attestation_score > C's attestation_score
     yield from add_attestation(spec, store, attestation, test_steps)
     assert spec.get_head(store) == signed_block_b.message.hash_tree_root()
+
+    yield 'steps', test_steps
+
+@with_all_phases
+@spec_state_test
+def test_ex_ante_CASPAR_sandwich_without_attestations_with_boost(spec, state):
+    """
+    Obejcts:
+        Block A - slot N
+        Block B (parent A) - slot N+1
+        Block C (parent A) - slot N+2
+        Block D (parent B) - slot N+3
+    Steps:
+        Block A received at N — A is head
+        Block C received at N+2 — C is head
+        Block B received at N+2 — C is head (with boost)
+        Block D received at N+3 — D is head (with boost)
+    """
+    test_steps = []
+    # Initialization
+    store, anchor_block = get_genesis_forkchoice_store_and_block(spec, state)
+    yield 'anchor_state', state
+    yield 'anchor_block', anchor_block
+    current_time = state.slot * spec.config.SECONDS_PER_SLOT + store.genesis_time
+    on_tick_and_append_step(spec, store, current_time, test_steps)
+    assert store.time == current_time
+    print("css test ||slot (initialization): ", spec.get_current_slot(store))
+    print("genesis time: ", store.genesis_time)
+
+    # On receiving block A at slot `N`
+    yield from _apply_base_block_a(spec, state, store, test_steps)
+    state_a = state.copy()
+    print("slot (received A): ", spec.get_current_slot(store))
+
+    # Block B at slot `N + 1`, parent is A
+    state_b = state_a.copy()
+    block = build_empty_block(spec, state_a, slot=state_a.slot + 1)
+    signed_block_b = state_transition_and_sign_block(spec, state_b, block)
+    print("B's slot is: ", signed_block_b.message.slot)
+    print("parent root: ", signed_block_b.message.parent_root)
+    print("root (block B): ", signed_block_b.message.hash_tree_root().hex())
+
+    # Block C at slot `N + 2`, parent is A
+    state_c = state_a.copy()
+    block = build_empty_block(spec, state_c, slot=state_a.slot + 2)
+    signed_block_c = state_transition_and_sign_block(spec, state_c, block)
+    print("C's slot is: ", signed_block_c.message.slot)
+    print("parent root: ", signed_block_c.message.parent_root)
+    print("root (block C): ", signed_block_c.message.hash_tree_root())
+
+    # Block D at slot `N + 3`, parent is B
+    state_d = state_b.copy()
+    block = build_empty_block(spec, state_d, slot=state_a.slot + 3)
+    signed_block_d = state_transition_and_sign_block(spec, state_d, block)
+    print("parent root: ", signed_block_d.message.parent_root)
+    print("root (block D): ", signed_block_d.message.hash_tree_root().hex())
+    
+    # Block C received at N+2 — C is head
+    time = state_c.slot * spec.config.SECONDS_PER_SLOT + store.genesis_time
+    print("C received: ", time)
+    on_tick_and_append_step(spec, store, time, test_steps)
+    print("slot (received C): ", spec.get_current_slot(store))
+    print("head view: ", spec.get_head(store))
+    print("before block C: get_head(store)", spec.get_head(store))
+    print("before block C: signed_block_c.message.hash_tree_root()", signed_block_c.message.hash_tree_root().hex())
+    yield from add_block(spec, store, signed_block_c, test_steps)
+    print("after block C: get_head(store)", spec.get_head(store))
+    print("after block C: signed_block_c.message.hash_tree_root()", signed_block_c.message.hash_tree_root().hex())
+    assert spec.get_head(store) == signed_block_c.message.hash_tree_root()
+
+    # Block B received at N+2 — C is head, it has proposer score boost
+    print("B received: ", time)
+    print("slot (received B): ", spec.get_current_slot(store))
+    print("before block B: get_head(store)", spec.get_head(store))
+    print("before block B: signed_block_b.message.hash_tree_root()", signed_block_b.message.hash_tree_root().hex())
+    yield from add_block(spec, store, signed_block_b, test_steps)
+    print("after block B: get_head(store)", spec.get_head(store))
+    print("after block B: signed_block_b.message.hash_tree_root()", signed_block_b.message.hash_tree_root().hex())
+    assert spec.get_head(store) == signed_block_c.message.hash_tree_root()
+
+    # Block D received at N+3 - D is head, it has proposer score boost
+    time = state_d.slot * spec.config.SECONDS_PER_SLOT + store.genesis_time
+    print("D received: ", time)
+    on_tick_and_append_step(spec, store, time, test_steps)
+    print("slot (received D): ", spec.get_current_slot(store))
+    print("head view: ", spec.get_head(store))
+    yield from add_block(spec, store, signed_block_d, test_steps)
+    assert spec.get_head(store) == signed_block_d.message.hash_tree_root()
 
     yield 'steps', test_steps
